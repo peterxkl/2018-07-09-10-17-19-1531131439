d08644638d1384a9493da2bfd83173af8911c9b2
Dillon <18711345569@163.com>
2019-07-11 21:31:36 +0800

    feat:finish pritice 1-7

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,21 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String  introduce(){
+        return super.introduce()+" I am a Student. I am at Class "+this.klass.getNumber()+".";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,32 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(this.klass==null){
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+        return super.introduce()+" I am a Teacher. I teach Class "+getKlass().getNumber()+".";
+    }
+
+    public String introduceWith(Student student){
+        int teacherClass=getKlass().getNumber();
+        int studentClass=student.getKlass().getNumber();
+        if(teacherClass==studentClass){
+            return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }
+        return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+    }
 }
